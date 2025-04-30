e7841b7439d195b6991643bc602c455df72c339b
Luky123456 <36738853+Luky123456@users.noreply.github.com>
2025-04-30 18:48:57 +0200

    Update buggy_python_code.py

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -26,12 +26,15 @@ def print_nametag(format_string, person):
 
 def fetch_website(urllib_version, url):
     # Import the requested version (2 or 3) of urllib
-    exec(f"import urllib{urllib_version} as urllib", globals())
+    allowed = [2,3]
+    if urllib_version in allowed:
+        exec(f"import urllib{urllib_version} as urllib", globals())
+    else:
+        print('Not allowed')
     # Fetch and print the requested URL
  
     try: 
         http = urllib.PoolManager()
-        r = http.request('GET', url)
     except:
         print('Exception')
 
